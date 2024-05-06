021773349a039aea3b9d1e561a4b4cd4640072dc
Abdel Ait Said <aaitsaid78@gmail.com>
2024-05-06 10:55:58 +0200

    Etape 3 fin: Intégration de la zone des photos apparentées en utilisant jquery. Le bloc d'affichage d'une photo qui servira pour la page d'accueil est fonctionnel

--- template-parts/photo_block.php
+++ template-parts/photo_block.php
@@ -3,28 +3,37 @@ $photo_url = get_the_post_thumbnail_url(null, "large");
 $photo_titre = get_the_title();
 $post_url = get_permalink();
 $reference = get_field('reference');
-$categories = get_the_terms(get_the_ID(), 'categorie');
-$categorie_name = $categories[0]->name;
 
+// Récupération de la catégorie
+$categories = get_the_terms(get_the_ID(), 'categorie');
+$categorie_name = '';
+if ($categories && !is_wp_error($categories)) {
+    $categorie_name = $categories[0]->name;
+}
 ?>
 
-            <div class="block_relative_photo">
-                <img src="<?php echo esc_url($photo_url); ?>" alt="<?php the_title_attribute(); ?>">
+<div class="blocPhotoSimilaire">
+    <img src="<?php echo esc_url($photo_url); ?>" alt="<?php the_title_attribute(); ?>">
 
-                <div class="overlay">
+    <div class="overlay">
+        <!-- Affichage du titre de la photo et de sa catégorie -->
+        <h2 class="Majuscule"><?php echo esc_html($photo_titre); ?></h2>
+        <h3 class="Majuscule"><?php echo esc_html($categorie_name); ?></h3>
 
-                    <h2 class="Majuscule"><?php echo esc_html($photo_titre); ?></h2>
-                    <h3 class="Majuscule"><?php echo esc_html($categorie_name); ?></h3>
+        <!-- Bouton pour voir la photo -->
+        <div class="eye-icon">
+            <a href="<?php echo esc_url($post_url); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/icon_eye.svg"
+                    alt="voir la photo">
+            </a>
+        </div>
 
-                    <div class="eye-icon">
-                        <a href="<?php echo esc_url(get_permalink()); ?>">
-                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/icon_eye.svg" alt="voir la photo">
-                        </a>
-                    </div>
-                    <div class="fullscreen-icon" data-full="<?php echo esc_url($photo_url); ?>" data-category="<?php echo esc_attr($categorie_name); ?>" data-reference="<?php echo esc_attr($reference); ?>">
-                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/fullscreen.svg" alt="Icone fullscreen">
-                    </div>
-                </div>
-            </div>
-            
-       
+        <!-- Bouton pour le mode plein écran -->
+        <div class="fullscreen-icon" data-full="<?php echo esc_url($photo_url); ?>"
+            data-category="<?php echo esc_attr($categorie_name); ?>"
+            data-reference="<?php echo esc_attr($reference); ?>">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/fullscreen.svg"
+                alt="Icone fullscreen">
+        </div>
+    </div>
+</div>
